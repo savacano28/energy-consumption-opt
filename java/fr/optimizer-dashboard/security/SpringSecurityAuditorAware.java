c91ac303b03782554f04eea539588acc4a022241
Savacano_28 <savacano28@users.noreply.github.com>
2020-11-10 10:06:55 +0100

    security

---
+++ java/fr/optimizer-dashboard/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,17 @@
+package fr.ifpen.synergreen.security;
+
+import fr.ifpen.synergreen.config.Constants;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        return SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM_ACCOUNT);
+    }
+}
