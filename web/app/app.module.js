b2a6788d2e835bcfcf98004d649893b49a72c5d5
Savacano_28 <savacano28@users.noreply.github.com>
2020-11-25 11:29:26 +0100

    app

---
+++ web/app/app.module.js
@@ -0,0 +1,29 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('synergreenApp', [
+            'ngStorage',
+            'tmh.dynamicLocale',
+            'pascalprecht.translate',
+            'ngResource',
+            'ngCookies',
+            'ngAria',
+            'ngCacheBuster',
+            'ngFileUpload',
+            'ui.bootstrap',
+            'ui.router',
+            'infinite-scroll',
+            'nvd3',
+            // jhipster-needle-angularjs-add-module JHipster will add new module here
+            'angular-loading-bar'
+        ])
+        .run(run);
+
+    run.$inject = ['stateHandler', 'translationHandler'];
+
+    function run(stateHandler, translationHandler) {
+        stateHandler.initialize();
+        translationHandler.initialize();
+    }
+})();
